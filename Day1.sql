55a805ef48b7ba3ef6b21f9ba2ee49776d89f2af
OmarInCS <omarincs@gmail.com>
2025-09-14 17:49:14 +0300

    init branch

---
+++ Day1.sql
@@ -0,0 +1,32 @@
+************ Day 1 ************** 
+Name: Omar Karem
+Mobile: 0599 888 921
+------------------------------------------
+
+
+* Retriving, Restricting and Sorting data
+
+
+
+---------------------- Examples ------------------------
+
+
+
+
+
+
+
+
+-------------------- Questions ------------------------
+
+* The HR department wants a query to display the last name, 
+job ID, hire date, and employee ID for each employee, 
+with the employee ID appearing first. Provide an alias
+STARTDATE for the HIRE_DATE column.
+
+* The HR department wants a query to display all 
+unique job IDs from the EMPLOYEES table.
+
+* Because of budget issues, the HR department needs 
+a report that displays the last name and salary 
+of employees who earn more than $12,000.
